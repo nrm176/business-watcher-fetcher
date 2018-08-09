181100f43c41f9340704a6fefcdd98624115d16c
shin37 <kojima37@gmail.com>
2018-08-09 16:00:48 +0900

    as of 080918

--- fetcher.py
+++ fetcher.py
@@ -9,6 +9,16 @@ from os.path import join, dirname
 import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from logging import getLogger, StreamHandler, DEBUG
+import hashlib
+import psycopg2
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
 
 keys = ['◎', '○', '□', '▲', '×']
 values = [1.0, 0.75, 0.5, 0.25, 0.0]
@@ -107,21 +117,27 @@ def create_dataframe(data, date, data_type='current'):
 
     df['日付'] = date
 
-    df['Id'] = generateHash(df)
+    # df['Id'] = generateHash(df)
 
     df = df.rename(columns=RENAME_COLUMNS)
-
-    df = df.set_index('id')
+    # df['id'] = generateHash(df[['dtype', 'category', 'reason', 'region', 'dt', 'reason_future', 'comments', 'industry', 'score_future', 'score_current']])
+    # df = df.set_index('id')
 
     return df
 
 
 def generateHash(df):
+
     hashes = []
     for idx, row in df.iterrows():
         xs = list(zip(row, row.index))
         t = tuple(tuple(x) for x in xs)
-        hashes.append(hash(t))
+
+        # h = hashlib.new(t)
+        r = ''.join(list(map(lambda t:str(t[0]), t)))
+        h = hashlib.md5(str.encode(r))
+
+        hashes.append(h.hexdigest())
     return hashes
 
 
@@ -145,6 +161,33 @@ def retrieve_csv_file(url):
 
 if __name__ == '__main__':
 
+    actual_dates = ['20020212', '20020311', '20020408', '20020514', '20020610', '20020708', '20020808', '20020910',
+                   '20021008', '20021111', '20021209', '20030116', '20030210', '20030310', '20030408', '20030513',
+                   '20030609', '20030708', '20030808', '20030908', '20031008', '20031111', '20031208', '20040115',
+                   '20040209', '20040308', '20040408', '20040514', '20040608', '20040708', '20040809', '20040908',
+                   '20041008', '20041109', '20041208', '20050114', '20050208', '20050308', '20050408', '20050513',
+                   '20050608', '20050708', '20050808', '20050908', '20051011', '20051109', '20051208', '20060113',
+                   '20060208', '20060308', '20060410', '20060512', '20060608', '20060710', '20060808', '20060908',
+                   '20061010', '20061109', '20061208', '20070112', '20070208', '20070308', '20070409', '20070510',
+                   '20070608', '20070709', '20070808', '20070910', '20071009', '20071108', '20071210', '20080111',
+                   '20080208', '20080310', '20080408', '20080512', '20080609', '20080708', '20080808', '20080908',
+                   '20081008', '20081111', '20081208', '20090113', '20090209', '20090309', '20090408', '20090513',
+                   '20090608', '20090708', '20090810', '20090908', '20091008', '20091110', '20091208', '20100112',
+                   '20100208', '20100308', '20100408', '20100513', '20100608', '20100708', '20100809', '20100908',
+                   '20101008', '20101109', '20101208', '20110112', '20110208', '20110308', '20110408', '20110512',
+                   '20110608', '20110708', '20110808', '20110908', '20111011', '20111109', '20111208', '20120112',
+                   '20120208', '20120308', '20120409', '20120510', '20120608', '20120709', '20120808', '20120910',
+                   '20121009', '20121108', '20121210', '20130111', '20130208', '20130308', '20130408', '20130510',
+                   '20130610', '20130708', '20130808', '20130909', '20131008', '20131111', '20131209', '20140114',
+                   '20140210', '20140310', '20140408', '20140512', '20140609', '20140708', '20140808', '20140908',
+                   '20141008', '20141111', '20141208', '20150113', '20150209', '20150309', '20150408', '20150513',
+                   '20150608', '20150708', '20150810', '20150908', '20151008', '20151110', '20151208', '20160112',
+                   '20160208', '20160308', '20160408', '20160512', '20160608', '20160708', '20160808', '20160908',
+                   '20161011', '20161109', '20161208', '20170112', '20170208', '20170308', '20170410', '20170511',
+                   '20170608', '20170710', '20170808', '20170908', '20171010', '20171109', '20171208', '20180112',
+                   '20180208', '20180308', '20180409', '20180510',
+                   '20180608', '20180709', '20180808']
+
     ON_HEROKU = os.environ.get("ON_HEROKU", False)
 
     file_path = '/tmp/%s_%s.csv' if ON_HEROKU else './%s_%s.csv'
@@ -154,29 +197,54 @@ if __name__ == '__main__':
 
     DATABASE_URL = os.environ["DATABASE_URL"]
 
-    today = datetime.strftime(datetime.today() - timedelta(days=1), '%Y-%m-%d')
+    dates = actual_dates[-100:-40]
+    # dates = ['20170112']
+
+    for dt in dates:
+        today_dt = datetime.strptime(dt, '%Y%m%d')
+        today = datetime.strftime(today_dt, '%Y-%m-%d')
+    # today = datetime.strftime(datetime.today() - timedelta(days=1), '%Y-%m-%d')
     # today = datetime.strftime(datetime.today(), '%Y-%m-%d')
 
-    url_dict = {
-        'outlook': 'http://www5.cao.go.jp/keizai3/%s/%s%swatcher/watcher5.csv' % tuple(today.split('-')),
-        'current': 'http://www5.cao.go.jp/keizai3/%s/%s%swatcher/watcher4.csv' % tuple(today.split('-'))
-    }
+        url_dict = {
+            'outlook': 'http://www5.cao.go.jp/keizai3/%s/%s%swatcher/watcher5.csv' % tuple(today.split('-')),
+            'current': 'http://www5.cao.go.jp/keizai3/%s/%s%swatcher/watcher4.csv' % tuple(today.split('-'))
+        }
 
-    dfs = []
-    for data_type, url in url_dict.items():
-        d = retrieve_csv_file(url)
-        if d:
-            df = create_dataframe(d, date=today, data_type=data_type)
-            dfs.append(df)
-            # df.to_csv('./%s_%s.csv' % (data_type, today), encoding='utf-8')
-        else:
-            print('%s not available' % url)
+        dfs = []
+        for data_type, url in url_dict.items():
+            logger.debug('doing %s' % url)
 
-    append_df = dfs[0].append(dfs[1], sort=False)
-    # concat_df = pd.concat(dfs, axis=1)
+            d = retrieve_csv_file(url)
+            if d:
+                df = create_dataframe(d, date=today_dt, data_type=data_type)
+                dfs.append(df)
+                # df.to_csv('./%s_%s.csv' % (data_type, today), encoding='utf-8')
+            else:
+                logger.error('%s not available' % url)
 
-    if ON_HEROKU:
+        if len(dfs) < 1:
+            logger.error('df is empty')
+
+            break
+        append_df = dfs[0].append(dfs[1], sort=False)
+        append_df['id'] = generateHash(append_df[['dtype', 'category', 'reason', 'region', 'dt', 'reason_future', 'comments',
+                                    'industry', 'score_future', 'score_current']])
+
+        # Remove unamed column
+        append_df = append_df.loc[:, ~append_df.columns.str.contains('^Unnamed')]
+
+        # append_df['dt'] = pd.to_datetime(df['dt'])
+        append_df = append_df.set_index('id')
+
+        # concat_df = pd.concat(dfs, axis=1)
+
+        # if ON_HEROKU:
         engine = create_engine(DATABASE_URL)
-        append_df.to_sql(os.environ['BUSINESS_WATCHER_BOT_TABLE_NAME'], engine, if_exists='append')
-    else:
+        try:
+            append_df.to_sql(os.environ['BUSINESS_WATCHER_BOT_TABLE_NAME'], engine, if_exists='append')
+        except Exception  as e:
+            logger.error('error on insert: {0}'.format(e))
+        # else:
         append_df.to_csv(file_path % ('append', today), encoding='utf-8')
+        logger.debug('saving at %s' % file_path % ('append', today))
